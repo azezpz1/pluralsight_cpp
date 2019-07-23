a8752b687017d1645ea73fb728cfefef80c1f53a
Anthony Panettiere <azezpz1@gmail.com>
2019-07-23 06:04:40 -0500

    Printing local variables i and j

--- Small/Small.cpp
+++ Small/Small.cpp
@@ -5,5 +5,14 @@ using namespace std;
 int main()
 {
 	cout << "Hello!" << endl << 2 + 2;
+
+	cout << endl << endl;
+
+	int i;
+	i = 3;
+	cout << i << endl;
+	
+	int j = 2;
+	cout << j << endl;
 	return 0;
 }
