fddaeb499a502bb4b9b1e8e3f0dad98b80cdb039
Anthony Panettiere <azezpz1@gmail.com>
2019-08-01 06:01:18 -0500

    Removing old math

--- Small/Small.cpp
+++ Small/Small.cpp
@@ -28,18 +28,5 @@ int main()
 		cout << "expected result." << endl;
 	}
 
-	double a = add(3, 4);
-	cout << "3 + 4 is " << a;
-
-	cout << endl;
-
-	double b = add(1.2, 3.4);
-	cout << "1.2 + 3.4 is " << b;
-
-	cout << endl;
-
-	double c = add(1.1, 2.2, 3.3);
-	cout << "1.1 + 2.2 + 3.3 is " << c;
-
 	return 0;
 }
