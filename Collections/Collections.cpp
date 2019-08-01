a80f1112aea65623b489f520a0098ea7086e060a
Anthony Panettiere <azezpz1@gmail.com>
2019-08-01 06:21:39 -0500

    Adding vector manipulations

--- Collections/Collections.cpp
+++ Collections/Collections.cpp
@@ -2,10 +2,41 @@
 //
 
 #include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm> //for sort and count
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	vector<int> vi;
+	for (int i = 0; i < 10; i++)
+	{
+		vi.push_back(i);
+	}
+
+	for (auto item:vi)
+	{
+		cout << item << " ";
+	}
+	cout << endl;
+
+	vector<string> vs;
+
+	cout << "enter three words ";
+	for (int i = 0; i < 3; i++)
+	{
+		string s;
+		cin >> s;
+		vs.push_back(s);
+	}
+
+	for (auto item : vs)
+	{
+		cout << item << " ";
+	}
+	cout << endl;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
