4cc6823da0837123bc53726f3d4e46c8642475a6
Anthony Panettiere <azezpz1@gmail.com>
2019-08-06 06:32:33 -0500

    Implemented simple banking classes

--- SimpleClasses/Account.h
+++ SimpleClasses/Account.h
@@ -1,1 +1,17 @@
 #pragma once
+#include <vector>
+#include <string>
+#include "Transaction.h"
+
+class Account
+{
+private:
+	int balance;
+	std::vector<Transaction> log;
+
+public:
+	Account();
+	std::vector<std::string> Report();
+	bool Deposit(int amt);
+	bool Withdraw(int amt);
+};
